976a0bcd35888ac58a77ed54eae4c1d2417b13dc
NeilChavez <lolochavez22@icloud.com>
2023-08-11 16:18:24 -0500

    feat: add Router

---
+++ Router.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MVC;
+
+class Router
+{
+
+  public $routesGET = [];
+
+  public function get($currentUrl, $fn)
+  {
+
+    $this->routesGET[$currentUrl] = $fn;
+  }
+
+  public function checkRoutes()
+  {
+
+    $currentUrl = $_SERVER[PATH_INFO] ?? ROUTE_HOME;
+    $method = $_SERVER[REQUEST_METHOD];
+
+    $fn = "";
+
+    if ($method === GET) {
+      $fn =  $this->routesGET[$currentUrl] ?? null;
+    }
+
+    if ($fn) {
+      call_user_func($fn, $this);
+    } else {
+      echo "PAGE NOT FOUND";
+    }
+  }
+
+  public function render($view, $args = [])
+  {
+
+    foreach ($args as $key => $value) {
+      $$key = $value;
+    }
+
+    ob_start();
+    include __DIR__ . "/views/pages/$view.php";
+    $content = ob_get_clean();
+    include __DIR__ . "/views/layout.php";
+  }
+}
