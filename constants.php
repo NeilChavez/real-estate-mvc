c3df07d69256c060e70020adecedd19511fc6d26
NeilChavez <lolochavez22@icloud.com>
2023-08-17 17:54:21 -0500

    feat: add constants

---
+++ constants.php
@@ -0,0 +1,28 @@
+<?php
+
+define("GET", "GET");
+define("POST", "POST");
+define("PATH_INFO", "PATH_INFO");
+define("REQUEST_METHOD", "REQUEST_METHOD");
+define("DOCUMENT_ROOT", "DOCUMENT_ROOT");
+
+//ROUTES CONSTS
+define("PROPERTIES", "/properties");
+define("ROUTE_HOME", "/");
+define("ROUTE_ADMIN", "/admin");
+define("ROUTE_CREATE", "/create");
+define("ROUTE_UPDATE", "/update");
+define("ROUTE_DELETE", "/delete");
+define("PROPERTIES_ROUTE_UPDATE", PROPERTIES . ROUTE_UPDATE);
+
+//DATABASE CONSTS
+define("LOCALHOST", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DB_NAME", "real_state");
+
+//CONTENT TEXT
+define("PAGE_TITLE", "Real State");
+
+
+?>
