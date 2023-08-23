2a40d1c30301a126292128abf078a668408db182
NeilChavez <lolochavez22@icloud.com>
2023-08-23 04:39:27 -0500

    feat: add helper functions

--- includes/functions.php
+++ includes/functions.php
@@ -6,5 +6,21 @@ function debugger($var){
   echo '</pre>'; 
   exit;
 }
-
+function mapMessageCodeToResult($entry)
+{
+  switch ($entry) {
+    case 1: {
+        return CREATE_SUCCESS;
+      }
+    case 2: {
+        return UPDATED_SUCCESS;
+      }
+    case 3: {
+        return DELETED_SUCCESS;
+      }
+    default: {
+        return "No Message was implemented to the case inserted";
+      }
+  }
+}
 ?>
