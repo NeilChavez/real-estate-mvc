a605a4dc61bc872cd162a96f3766e341aac12e64
NeilChavez <lolochavez22@icloud.com>
2023-09-02 17:38:41 -0500

    refactor: centralize the common methods of the Agent and Property models in the ActiveRecord class

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -5,7 +5,6 @@ namespace Model;
 class ActiveRecord
 {
 
-
   //data base 
   protected static $db;
   protected static $columnsDB = [];
@@ -13,6 +12,7 @@ class ActiveRecord
 
   // properties
   public $id;
+  public $image;
 
   //errors 
   protected static $errors = [];
@@ -45,6 +45,144 @@ class ActiveRecord
     }
   }
 
+  public function update()
+  {
+
+    $attributes = $this->getAttributes();
+
+    $values = [];
+
+    foreach ($attributes as $key => $value) {
+      $values[] = "$key='$value'";
+    }
+
+    $values = join(", ", $values);
+
+    $query = "UPDATE " . static::$table . " SET " . $values . " WHERE id=$this->id";
+
+
+    $result = self::$db->query($query);
+
+    if ($result) {
+      $code = CODE_UPDATED_SUCCESS;
+      $direction = ROUTE_ADMIN . "?code_message=" . $code;
+      header("Location: " . $direction);
+    } else {
+      echo "NOT UPDATED IN DB";
+    }
+  }
+
+  public function delete()
+  {
+
+    $query = "DELETE FROM " . static::$table . " WHERE id=$this->id";
+
+    $result = self::$db->query($query);
+
+    if ($result) {
+      //delete image in server
+      $this->deleteImg();
+
+      //redirect the user
+      $code = CODE_DELETED_SUCCESS;
+      $direction = ROUTE_ADMIN . "?code_message=" . $code;
+      header("Location: " . $direction);
+    } else {
+      echo "NOT DELETED IN DB :( ";
+    }
+  }
+
+  public static function getAll()
+  {
+
+    $query = "SELECT * FROM " . static::$table;
+
+    $results = self::consultSQL($query);
+
+    return $results;
+  }
+
+  public static function findById($id)
+  {
+    $query = "SELECT * FROM " . static::$table . " WHERE id=$id";
+
+    $result = self::consultSQL($query);
+
+    return array_shift($result);
+  }
+
+
+  public function sincronize($args)
+  {
+
+    foreach ($args as $key => $value) {
+
+      if (property_exists($this, $key) && !is_null($value)) {
+
+        $this->$key = $value;
+      }
+    }
+  }
+
+  public function setImage($image)
+  {
+    //if we have an id we are updating
+    if (!is_null($this->id)) {
+
+      //delete image from server
+      $this->deleteImg();
+    }
+
+    //and in all the cases set the image property to the instance of this class
+    $this->image = $image;
+  }
+
+  public static function consultSQL($query)
+  {
+
+    $values = [];
+
+    $results = self::$db->query($query);
+
+    if ($results) {
+
+      while ($row = $results->fetch_assoc()) {
+        $values[] = self::createObject($row);
+      }
+    }
+
+    return $values;
+  }
+
+  public static function createObject($entry)
+  {
+
+    $entity = new static;
+
+    //iterate for each column of the single row
+    foreach ($entry as $key => $value) {
+
+      //check if the object has a property and set the value if it's true
+
+      if (property_exists($entity, $key) && $value !== null) {
+        $entity->$key = $value;
+      }
+    }
+
+    return $entity;
+  }
+
+  public function deleteImg()
+  {
+    if (is_dir(PATH_IMAGES)) {
+
+      //this is the file position
+      $fileImage = PATH_IMAGES . $this->image;
+
+      //check it exists and delete
+      file_exists($fileImage) && unlink($fileImage);
+    }
+  }
 
   public function getAttributes()
   {
--- models/Agent.php
+++ models/Agent.php
@@ -16,7 +16,7 @@ class Agent extends ActiveRecord
   public $name_agent;
   public $surname_agent;
   public $phone_number;
-  public $image_agent;
+  public $image;
 
   public function __construct($args = [])
   {
@@ -24,48 +24,7 @@ class Agent extends ActiveRecord
     $this->name_agent =  $args["name_agent"] ?? "";
     $this->surname_agent = $args["surname_agent"] ?? "";
     $this->phone_number = $args["phone_number"] ?? "";
-    $this->image_agent = $args["image_agent"] ?? "";
-  }
-
-
-  public function update()
-  {
-
-    $attributes = $this->getAttributes();
-
-    $values = [];
-    foreach ($attributes as $key => $value) {
-      $values[] = "$key='$value'";
-    }
-    $values = join(", ", $values);
-
-    $query = "UPDATE " . self::$table . " SET " . $values . " WHERE id=$this->id;";
-
-    $result = self::$db->query($query);
-
-    if ($result) {
-      $code = CODE_UPDATED_SUCCESS;
-      $direction = ROUTE_ADMIN . "?code_message=" . $code;
-      header("Location: " . $direction);
-    } else {
-      echo "NOT UPDATED IN DB";
-    }
-  }
-  public function delete()
-  {
-
-    $query = "DELETE FROM " . self::$table . " WHERE id=$this->id;";
-    
-    $result = self::$db->query($query);
-
-    if ($result) {
-      $code = CODE_DELETED_SUCCESS;
-      $direction = ROUTE_ADMIN . "?code_message=" . $code;
-      header("Location: " . $direction);
-    } else {
-      echo "NOT DELETED IN DB";
-    }
-
+    $this->image = $args["image_agent"] ?? "";
   }
 
   public function validate()
@@ -87,90 +46,11 @@ class Agent extends ActiveRecord
       self::$errors[] = "You need to insert a 'valid' phone number";
     }
 
-    if ($this->image_agent === "") {
+    if ($this->image === "") {
       self::$errors[] = "You need to insert an image";
     }
 
     return self::$errors;
   }
 
-  public function setImage($image)
-  {
-    //if the object instance of this class has an id, it means we are updating
-    if (!is_null($this->id)) {
-      //delete image from the server
-
-      //this is the file position
-      $pathFile = PATH_IMAGES . $this->image_agent;
-
-      //check it exists and delete
-      file_exists($pathFile) &&  unlink($pathFile);
-    }
-
-    //set the image property
-    $this->image_agent = $image;
-  }
-
-  public static function getAll()
-  {
-
-    $query = "SELECT * FROM agents";
-
-    $agents = self::consultSQL($query);
-
-    return $agents;
-  }
-
-  public static function findById($id)
-  {
-    $query = "SELECT * FROM " . self::$table . " WHERE id=$id";
-
-    $result = self::consultSQL($query);
-
-    return array_shift($result);
-  }
-
-  public static function consultSQL($query)
-  {
-
-    $result =  self::$db->query($query);
-
-    $agents = [];
-
-    if ($result) {
-      while ($row = $result->fetch_assoc()) {
-
-        $agents[] = self::createObject($row);
-      }
-    }
-
-    return $agents;
-  }
-
-  public static function createObject($singleRow = [])
-  {
-
-    $singleAgent = new self;
-
-    //iterate for each column of the single row
-    foreach ($singleRow as $key => $value) {
-
-      //check if the object has a property and set the value if it's true
-      if (property_exists($singleAgent, $key)) {
-
-        $singleAgent->$key = $value;
-      }
-    }
-    return $singleAgent;
-  }
-
-  public function sincronize($args = [])
-  {
-
-    foreach ($args as $key => $value) {
-      if (property_exists($this, $key) && !is_null($value)) {
-        $this->$key = $value;
-      }
-    }
-  }
 }
