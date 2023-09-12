7244e9c4da49df6f9dabf5be7f777a60d42b3d94
NeilChavez <lolochavez22@icloud.com>
2023-09-12 10:49:30 -0500

    feat: update layout

--- views/layout.php
+++ views/layout.php
@@ -23,7 +23,7 @@
       <div class="menu" id="menu">
         <ul class="list">
           <li class="list-item">
-            <a href="#" class="list-link current">Properties</a>
+            <a href="<?php echo ROUTE_PROPERTIES?>" class="list-link current">Properties</a>
           </li>
           <li class="list-item">
             <a href="#" class="list-link">About Us</a>
@@ -34,10 +34,6 @@
           <li class="list-item">
             <a href="#" class="list-link">Contact Us</a>
           </li>
-          <li class="list-item">
-            <a href="#" class="list-link">Contact</a>
-          </li>
-
           <?php
           $condition = true;
           // if (isset($_SESSION)) {
@@ -70,7 +66,82 @@
 
   <?php echo $content ?>
 
-  <footer>this is a footer</footer>
+  <footer class="footer section">
+    <div class="footer-container container d-grid">
+      <div class="company-data flex">
+        <a href="/">
+          <h2 class="logo">Real State</h2>
+        </a>
+        <p class="company-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          assumenda nulla sit amet consectetur adipisicing elit. Facere
+          assumenda nulla
+        </p>
+        <ul class="list social-media">
+          <img src="/images/icons/twitch-icon.svg" alt="twitch icon" class="icon">
+          <img src="/images/icons/twitter-icon.svg" alt="twitter icon" class="icon">
+          <img src="/images/icons/facebook-icon.svg" alt="facebook icon" class="icon">
+        </ul>
+      </div>
+      <div class="list-footer-categories">
+        <div>
+          <h6 class="title footer-title">Categories</h6>
+          <ul class="list footer-list">
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 class="title footer-title">useful links</h6>
+          <ul class="list footer-list">
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h6 class="title footer-title">Company</h6>
+          <ul class="list footer-list">
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Simple Item</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <span class="copyright-notice">&copy; 2023 Real State. All rights reserved
+    </span>
+  </footer>
 
   <script src="/src/js/index.js" type="module"></script>
 </body>
