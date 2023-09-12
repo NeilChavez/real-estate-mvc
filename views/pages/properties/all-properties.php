e5557f6f4e7c65ab166111772b4c36ede885acaf
NeilChavez <lolochavez22@icloud.com>
2023-09-12 11:02:14 -0500

    feat: add all properties section

---
+++ views/pages/properties/all-properties.php
@@ -0,0 +1,8 @@
+<section class="properties-section" id="section-properties">
+  <h2 class="title-section">Properties for sale in your favorite area</h2>
+  <div class="fluid-grid container">
+    <?php
+    include __DIR__."/../list-properties.php";
+    ?>
+  </div>
+</section>
