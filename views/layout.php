6953fe756f35dcac7069b90c1181a19ab6ff6cf7
NeilChavez <lolochavez22@icloud.com>
2023-09-18 05:56:40 -0500

    feat: add session in layout

--- views/layout.php
+++ views/layout.php
@@ -1,3 +1,13 @@
+<?php
+
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$auth = $_SESSION["login"] ?? null;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,53 +28,53 @@
   <header class="header" id="header">
     <nav class="navbar container">
       <a href="<?php echo ROUTE_HOME ?>">
-        <h1 class="logo"><?php echo PAGE_TITLE ?></h1>
+        <h1 class="logo">
+          <?php echo PAGE_TITLE ?>
+        </h1>
       </a>
-      <div class="menu" id="menu">
-        <ul class="list">
-          <li class="list-item">
-            <a href="<?php echo ROUTE_PROPERTIES?>" class="list-link current">Properties</a>
-          </li>
-          <li class="list-item">
-            <a href="#" class="list-link">About Us</a>
-          </li>
-          <li class="list-item">
-            <a href="#" class="list-link">Blog</a>
-          </li>
-          <li class="list-item">
-            <a href="#" class="list-link">Contact Us</a>
-          </li>
-          <?php
-          $condition = true;
-          // if (isset($_SESSION)) {
-          if ($condition) {
-          ?>
+      <div>
+        <div class="menu" id="menu">
+          <ul class="list">
             <li class="list-item">
-              <a href="<?php echo ROUTE_ADMIN ?>" class="list-link">
-                Admin dashboard
-              </a>
+              <a href="<?php echo ROUTE_PROPERTIES ?>" class="list-link current">Properties</a>
             </li>
             <li class="list-item">
-              <a href="<?php echo ROUTE_LOGOUT ?>" class="list-link btn-sm btn-blue">Logout</a>
+              <a href="#" class="list-link">About Us</a>
             </li>
+            <li class="list-item">
+              <a href="#" class="list-link">Contact Us</a>
+            </li>
+            <?php
+            if ($auth) {
+              ?>
+              <li class="list-item">
+                <a href="<?php echo ROUTE_ADMIN ?>" class="list-link">
+                  Admin dashboard
+                </a>
+              </li>
+              <li class="list-item">
+                <a href="<?php echo ROUTE_LOGOUT ?>" class="list-link btn-sm btn-blue">Logout</a>
+              </li>
 
-          <?php
-          } ?>
+              <?php
+            } ?>
 
-        </ul>
-      </div>
-      <div class="list list-right flex flex-row gap">
-        <div class="hamburger" id="hamburger-1">
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
+          </ul>
+        </div>
+        <div class="list list-right flex flex-row gap">
+          <div class="hamburger" id="hamburger-1">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+          </div>
         </div>
       </div>
     </nav>
   </header>
 
-
-  <?php echo $content ?>
+  <main>
+    <?php echo $content ?>
+  </main>
 
   <footer class="footer section">
     <div class="footer-container container d-grid">
