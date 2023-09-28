8fc67692a21211a4b0877bf12faeae385e6637a5
NeilChavez <lolochavez22@icloud.com>
2023-09-28 10:29:36 -0500

    feat: add call to dotenv in app

--- includes/app.php
+++ includes/app.php
@@ -7,6 +7,9 @@ include __DIR__ . "/../constants.php";
 include __DIR__ . "/config/db.php";
 include __DIR__ . "/functions.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 $connection = getConnectionDB();
 
 ActiveRecord::setDB($connection);
