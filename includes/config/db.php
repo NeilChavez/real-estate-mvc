26fbe5597fd0f8b749f802ef2ffc87e7a7913de3
NeilChavez <lolochavez22@icloud.com>
2023-09-30 05:20:08 -0500

    fix: fix db config

--- includes/config/db.php
+++ includes/config/db.php
@@ -3,7 +3,9 @@
 function getConnectionDB()
 {
 
-  $connection = new mysqli(LOCALHOST, USERNAME, PASSWORD, DB_NAME);
+   $connection = new mysqli($_ENV["DB_LOCALHOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+   //in localhost
+  // $connection = new mysqli("localhost", "root", "", "real_state");
 
   $connection->set_charset("utf8");
 
